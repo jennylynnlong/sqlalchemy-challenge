74142f1a16965552e47364a90de58439d7a4aaa5
jennylynnlong <88349512+jennylynnlong@users.noreply.github.com>
2021-11-20 22:57:42 -0500

    still need to work on start and end dates

--- app.py
+++ app.py
@@ -44,12 +44,12 @@ def welcome():
     )
 
 @app.route("/api/v1.0/precipitation")
-def names():
+def precip():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
+    """Return a list of all dates and precipitation in last year of data"""
+    # Query dates and precipitation of last year of data
     results = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date > dt.date(2016, 8, 22)).all()
 
@@ -59,3 +59,78 @@ def names():
     all_prcp = list(np.ravel(results))
 
     return jsonify(all_prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all the stations"""
+    # Query all stations
+    results = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of dates and temps of most active station in last year of data"""
+    # Query most active station's dates and temps in last year of data
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > dt.date(2016, 8, 22)).\
+        filter(Measurement.station == "USC00519281").all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    annualMostActive = list(np.ravel(results))
+
+    return jsonify(annualMostActive)
+
+@app.route("/api/v1.0/start")
+def start_date(start=""):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of min, avg, and max temps for a given start date"""
+    # Query min, avg, and max temps for a given start date
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    start = dt.datetime.strptime(start, "%m%d%Y")
+    results = session.query(*sel).filter(Measurement.date >= start).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    start_results = list(np.ravel(results))
+
+    return jsonify(start_results)
+
+@app.route("/api/v1.0/start/end")
+def start_and_end(start="", end=""):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of min, avg, and max temps for between given start and end dates"""
+    # Query min, avg, and max temps for between given start and end dates
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    start = dt.datetime.strptime(start, "%m%d%Y")
+    end = dt.datetime.strptime(end, "%m%d%Y")
+    results = session.query(*sel).filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    start_and_end_results = list(np.ravel(results))
+
+    return jsonify(start_and_end_results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
